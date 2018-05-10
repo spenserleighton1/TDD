f0dd19e024d5c6236a99289991b0f3123eafbf84
Spenser Leighton <spenser.leighton@gmail.com>
2018-05-10 12:15:28 -0600

    Write tests for sorting suite

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,11 @@
 import { assert } from 'chai';
+let Index = require('../lib/index.js');
 
-  describe('test', function() {
-    it('should return true', function() {
-      assert.equal(true, true);
+  describe('insertion sort', function() {
+    it('should sort an array of numbers', function() {
+      let actual = [1,3,2]
+      let expected = [1,2,3]
+      insertion(actual)
+      assert.deepEqual(actual, expected);
     });
   });
